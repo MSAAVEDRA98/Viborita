1ca83578aefc0dd995e9802910c0a4aaf2710822
MSAAVEDRA98 <simonsnow98@gmail.com>
2023-05-10 14:48:46 -0300

    Se mueve solo y direccion con las felchas

--- Viborita/src/app/componente1/componente1.component.ts
+++ Viborita/src/app/componente1/componente1.component.ts
@@ -7,30 +7,54 @@ import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
 })
 export class Componente1Component implements OnInit {
 
+  private intervalId: any;
+  private direccionX = -1;
+  private direccionY = 0;
+
   constructor(private elementRef: ElementRef) { }
 
-  @HostListener('window:keydown', ["$event"])
+  @HostListener('window: keydown', ['$event'])
 
-  moverElemento(evento: KeyboardEvent){
-    const elemento = this.elementRef.nativeElement.querySelector("#objeto")
-
-    if(elemento){
-    let posicion = parseInt(elemento.style.left || '0', 10)
-
-    if(evento.key === "ArrowLeft"){
-      posicion -= 10;
+  onKeyPress(event: KeyboardEvent){
+    if(event.key === 'ArrowUp'){
+      this.direccionY = -1
     }
-    if(evento.key === "ArrowRight"){
-      posicion += 10;
+    if(event.key === 'ArrowDown'){
+      this.direccionY = 1
     }
-
-    elemento.style.left = `${posicion}px`;
-  }
-
-
+    if(event.key === 'ArrowLeft'){
+      this.direccionX = -1
+    }
+    if(event.key === 'ArrowRight'){
+      this.direccionX = 1
+    }
   }
 
   ngOnInit(): void {
+    const elemento = this. elementRef.nativeElement.querySelector('#objeto');
+
+    this.intervalId = setInterval(() => {
+      let posicionX = parseInt(elemento.style.left || '0', 10);
+      let posicionY = parseInt(elemento.style.top || '0', 10);
+
+      if (posicionX >= window.innerWidth - elemento.offsetWidth || posicionX <= 0) {
+        this.direccionX *= -1;
+      }
+      if (posicionY >= window.innerHeight - elemento.offsetHeight || posicionY <= 0) {
+        this.direccionY *= -1;
+      }
+
+      posicionX += this.direccionX * 10;
+      posicionY += this.direccionY * 10;
+
+      elemento.style.left = `${posicionX}px`;
+      elemento.style.top = `${posicionY}px`;
+
+    },500)
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
   }
 
 }
